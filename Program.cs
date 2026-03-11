ce61baa3276bc26686b7742f47a6b8b9e6834b4d
desolami <99566510+desolami@users.noreply.github.com>
2026-03-11 02:12:20 +0100

    Add files via upload
    # C# Grade Calculator
    
    This project is a beginner-friendly C# console application that calculates a student's grade based on the score entered by the user.
    
    The program validates user input and assigns a grade using conditional statements.
    
    ## Features
    
    - Accepts user score input
    - Validates input using TryParse
    - Assigns grades using if/else conditions
    - Allows users to check another score
    
    ## Concepts Practiced
    
    - Variables and data types
    - User input with Console.ReadLine()
    - Conditional statements (if / else)
    - Input validation with TryParse
    - While loops
    
    ## How to Run
    
    1. Clone the repository
    2. Open in Visual Studio or VS Code
    3. Run the program
    
    ## Author
    
    Zainab

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+
+double num = 0;
+Console.WriteLine("Enter your score: ");
+while (!double.TryParse(Console.ReadLine(), out num))
+{
+
+    Console.WriteLine("Invalid Score. Please enter a number");
+}
+
+string grade = "";
+if (num >= 70 && num <= 100)
+{
+
+    grade = "A";
+}
+else if (num >= 60 && num <= 69)
+{
+
+    grade = "B";
+}
+else if (num >= 50 && num <= 59)
+{
+
+    grade = "C";
+}
+else if (num >= 45 && num <= 49)
+{
+    grade = "D";
+}
+
+else if (num >= 40 && num <= 44)
+{
+    grade = "E";
+}
+
+else if (num <= 40)
+{
+    grade = "F";
+}
+
+Console.WriteLine($"Your score : {num}");
+Console.WriteLine($"Your grade : {grade}");
+
+string answer = "yes";
+
+while (answer == "yes")
+{
+  
+
+    Console.WriteLine("Do you want to check another score? (yes/no)");
+    answer = Console.ReadLine();
+}
+
+Console.WriteLine("Goodbye!");
+
+
+
+
+
